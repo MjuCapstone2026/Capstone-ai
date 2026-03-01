e667c6db2de4182f3d8505c9878622611e1abbd8
ChaeHyunLim <chaehyun010104@gmail.com>
2026-03-01 23:34:50 +0900

    setting : swagger 설정

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 
-app = FastAPI(title="MJU Capstone AI AGENT")
+app = FastAPI(
+    title="MJU Capstone AI AGENTI",
+    description="명지대학교 자연캠퍼스 가이드 및 여행 일정을 짜주는 AI 에이전트 서버입니다.",
+    version="1.0.0",
+    docs_url="/docs",
+    redoc_url="/redoc",
+)
 
 @app.get("/")
 async def root():
